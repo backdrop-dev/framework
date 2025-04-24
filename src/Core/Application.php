1e542a5d0da01b991616bddc94ecead34f5fc0c9
techkodeninja <164923145+techkodeninja@users.noreply.github.com>
2025-04-24 11:26:16 -0700

    UNDEFINED

--- src/Core/Application.php
+++ src/Core/Application.php
@@ -105,7 +105,7 @@ class Application extends Container implements ApplicationContract, Bootable {
 		$this->instance( 'app', $this );
 
 		// Adds the directory path for the framework.
-		$this->instance( 'path', untrailingslashit( HYBRID_DIR  ) );
+		$this->instance( 'path', untrailingslashit( BACKDROP_DIR  ) );
 
 		// Add the version for the framework.
 		$this->instance( 'version', static::VERSION );
