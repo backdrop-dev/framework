118be45ecfb689fd74183fe067556f57f58a84bd
techkodeninja <164923145+techkodeninja@users.noreply.github.com>
2025-04-24 15:00:19 -0700

    rather than using get_current_blog_id, use bloginfo for multiste

--- src/functions-filters.php
+++ src/functions-filters.php
@@ -342,7 +342,7 @@ function body_class_filter( $classes, $class ) {
 	// Multisite check adds the 'multisite' class and the blog ID.
 	if ( is_multisite() ) {
 		$classes[] = 'multisite';
-		$classes[] = 'blog-' . get_current_blog_id();
+		$classes[] = 'blog-' . sanitize_html_class( sanitize_title( get_bloginfo( 'name' ) ) );
 	}
 
 	// Plural/multiple-post view (opposite of singular).
