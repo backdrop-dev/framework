6a889da816a98123280ba2123ad87583119b4a7d
techkodeninja <164923145+techkodeninja@users.noreply.github.com>
2025-04-24 13:34:45 -0700

    update render_title for Site

--- src/Site/functions-site.php
+++ src/Site/functions-site.php
@@ -37,9 +37,9 @@ function display_title( array $args = [] ) {
 function render_title( array $args = [] ) {
 
 	$args = wp_parse_args( $args, [
-		'tag'        => is_front_page() ? 'h1' : 'div',
-		'class'      => 'app-header__title',
-		'link_class' => 'app-header__title-link'
+		'tag'        => 'h1',
+		'class'      => 'site-header-title',
+		'link_class' => 'site-header-title-link'
 	] );
 
 	$html  = '';
