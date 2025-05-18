9e6478829239a3b7364a5ccb823e29aaa50135f2
techkodeninja <164923145+techkodeninja@users.noreply.github.com>
2025-05-18 16:21:52 -0700

    update

--- src/Site/functions-site.php
+++ src/Site/functions-site.php
@@ -39,7 +39,7 @@ function render_title( array $args = [] ) {
 	$args = wp_parse_args( $args, [
 		'class'      => 'site-header__title',
 		'link_class' => 'site-header__title-link',
-		'tag'        => is_front_page() ? 'h1' : 'div',
+		'tag'        => 'h1'
 	] );
 
 	$html  = '';
