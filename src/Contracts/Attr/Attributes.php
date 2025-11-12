5c7a3af3db85124951c33fd3985d2dec52554d54
techkodeninja <164923145+techkodeninja@users.noreply.github.com>
2025-11-12 13:26:25 -0800

    update file

--- src/Contracts/Attr/Attributes.php
+++ src/Contracts/Attr/Attributes.php
@@ -30,30 +30,41 @@ interface Attributes extends Renderable, Displayable {
 	 * Returns an array of HTML attributes in name/value pairs. Attributes
 	 * are not expected to be escaped. Escaping should be handled on output.
 	 *
+	 * - `array` return type hint introduced in PHP 7.0
+	 *
 	 * @since  1.0.0
 	 * @access public
 	 * @return array
 	 */
-	public function all();
+	public function all(): array;
+
 
 	/**
 	 * Returns a single, unescaped attribute's value.
 	 *
+	 * - `string` type hint introduced in PHP 7.0
+	 *
 	 * @since  1.0.0
 	 * @access public
-	 * @param  string  $name
+	 * @param  string  $name  The name of the attribute.
 	 * @return string
 	 */
-	public function get( $name );
+	public function get( string $name ): string;
+
 
 	/**
 	 * Adds custom data to the attribute object.
 	 *
+	 * - `string` and `array` type hints introduced in PHP 7.0
+	 * - Return type `$this` (fluent interface) supported since PHP 5.0
+	 * - `mixed` type not available until PHP 8.0, so only documented here
+	 *
 	 * @since  1.0.0
 	 * @access public
-	 * @param  string|array  $name
-	 * @param  mixed         $value
-	 * @return $this
+	 * @param  string|array  $key    The attribute key or an array of key/value pairs.
+	 * @param  mixed         $value  The value to assign (optional).
+	 * @return static
 	 */
-	public function with( $key, $value = null );
+	public function with( string|array $key, $value = null ): static;
+
 }
