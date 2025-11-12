1f5a9122968e5406348530c0f79f2d5687c270da
techkodeninja <164923145+techkodeninja@users.noreply.github.com>
2025-11-12 14:37:59 -0800

    update file

--- src/Contracts/Pagination/Pagination.php
+++ src/Contracts/Pagination/Pagination.php
@@ -4,6 +4,9 @@
  *
  * Defines the interface that pagination classes must use.
  *
+ * Compatible with PHP 8.0+ (uses return type declarations).
+ * - `self` and interface return types supported since PHP 7.0
+ *
  * @package   Backdrop
  * @author    Benjamin Lu <benlumia007@gmail.com>
  * @copyright 2019 Benjamin Lu
@@ -25,11 +28,14 @@ use Backdrop\Contracts\Displayable;
 interface Pagination extends Renderable, Displayable {
 
 	/**
-	 * Builds the pagination.
+	 * Builds the pagination instance.
+	 *
+	 * Compatible with PHP 8.0+ (typed return value).
+	 * - Interface return types supported since PHP 7.0
 	 *
 	 * @since  1.0.0
 	 * @access public
 	 * @return Pagination
 	 */
-	public function make();
+	public function make(): Pagination;
 }
