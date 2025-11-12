6d7d4674cfcea1cb1d0abf7e05438e16d2f18ba8
techkodeninja <164923145+techkodeninja@users.noreply.github.com>
2025-11-12 14:35:36 -0800

    update Contracts for Language

--- src/Contracts/Lang/Language.php
+++ src/Contracts/Lang/Language.php
@@ -4,6 +4,10 @@
  *
  * Defines the contract that a language class should use.
  *
+ * Compatible with PHP 8.0+ (uses scalar and `void` type hints).
+ * - `string` type hint introduced in PHP 7.0
+ * - `void` return type introduced in PHP 7.1
+ *
  * @package   Backdrop
  * @author    Benjamin Lu <benlumia007@gmail.com>
  * @copyright 2019 Benjamin Lu
@@ -30,7 +34,7 @@ interface Language extends Bootable {
 	 * @access public
 	 * @return string
 	 */
-	public function parentTextdomain();
+	public function parentTextdomain(): string;
 
 	/**
 	 * Returns the child theme textdomain.
@@ -39,27 +43,33 @@ interface Language extends Bootable {
 	 * @access public
 	 * @return string
 	 */
-	public function childTextdomain();
+	public function childTextdomain(): string;
 
 	/**
 	 * Returns the full directory path for the parent theme's domain path
 	 * and should allow a file/path to be appended.
 	 *
+	 * Compatible with PHP 8.0+ (uses scalar type hints).
+	 * - `string` type hint introduced in PHP 7.0
+	 *
 	 * @since  1.0.0
 	 * @access public
-	 * @param  string  $file
+	 * @param  string  $file  Optional file or subpath to append.
 	 * @return string
 	 */
-	public function parentPath( $file = '' );
+	public function parentPath( string $file = '' ): string;
 
 	/**
 	 * Returns the full directory path for the child theme's domain path
 	 * and should allow a file/path to be appended.
 	 *
+	 * Compatible with PHP 8.0+ (uses scalar type hints).
+	 * - `string` type hint introduced in PHP 7.0
+	 *
 	 * @since  1.0.0
 	 * @access public
-	 * @param  string  $file
+	 * @param  string  $file  Optional file or subpath to append.
 	 * @return string
 	 */
-	public function childPath( $file = '' );
+	public function childPath( string $file = '' ): string;
 }
