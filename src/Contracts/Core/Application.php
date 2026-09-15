077c27a369f09ec1817a84ae4243de64ed3676f1
techkodeninja <164923145+techkodeninja@users.noreply.github.com>
2026-09-15 16:57:56 -0700

    Add boot method to application contract

--- src/Contracts/Core/Application.php
+++ src/Contracts/Core/Application.php
@@ -30,14 +30,16 @@ interface Application extends Container {
 	 * @since  1.0.0
 	 * @access public
 	 *
-	 * @param  mixed $provider A service-provider instance or class name.
+	 * @param  mixed $provider A service provider instance or class name.
 	 * @return void
 	 */
 	public function provider( $provider ): void;
 
 	/**
-	 * Adds a static proxy alias. Developers must pass in a fully qualified
-	 * class name and an alias class name.
+	 * Adds a static proxy alias.
+	 *
+	 * Developers must pass in a fully qualified class name and an alias class
+	 * name.
 	 *
 	 * @since  1.0.0
 	 * @access public
@@ -47,4 +49,17 @@ interface Application extends Container {
 	 * @return void
 	 */
 	public function proxy( string $class_name, string $alias ): void;
+
+	/**
+	 * Boots the application.
+	 *
+	 * Registers and boots the application's service providers and registers
+	 * the configured static proxies.
+	 *
+	 * @since  1.0.0
+	 * @access public
+	 *
+	 * @return void
+	 */
+	public function boot(): void;
 }
