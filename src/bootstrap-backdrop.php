6b879da8a6d6611f7b4ccecce6fedb11b6ae3319
techkodeninja <164923145+techkodeninja@users.noreply.github.com>
2026-09-15 16:45:23 -0700

    Fix bootstrap file paths

--- src/bootstrap-backdrop.php
+++ src/bootstrap-backdrop.php
@@ -1,9 +1,9 @@
 <?php
 /**
- * Framwork bootstrap process.
+ * Framework bootstrap process.
  *
  * This file bootstraps parts of the framework that can't be autoloaded. We
- * define any global constants here and load any additional functions files.
+ * define any global constants here and load any additional function files.
  *
  * @package   Backdrop
  * @author    Benjamin Lu <benlumia007@gmail.com>
@@ -12,18 +12,21 @@
  * @link      https://github.com/backdrop-dev/framework
  */
 
-# Define the directory path to the framework. This shouldn't need changing
-# unless doing something really out there or just for clarity.
+/**
+ * Define the directory path to the framework.
+ */
 if ( ! defined( 'BACKDROP_DIR' ) ) {
-
 	define( 'BACKDROP_DIR', __DIR__ );
 }
 
-# Check if the framework has been bootstrapped. If not, load the bootstrap files
-# and get the framework set up.
+/**
+ * Bootstrap the framework.
+ *
+ * Load the framework functions and define the bootstrap constant so the
+ * bootstrap process only runs once.
+ */
 if ( ! defined( 'BACKDROP_BOOTSTRAPPED' ) ) {
-
-	require_once( 'bootstrap-functions.php' );
+	require_once __DIR__ . '/bootstrap-functions.php';
 
 	define( 'BACKDROP_BOOTSTRAPPED', true );
 }
