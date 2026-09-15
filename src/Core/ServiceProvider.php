792e36ed5c0d4cc1d69cfa91a7d52dd917c2cacb
techkodeninja <164923145+techkodeninja@users.noreply.github.com>
2026-09-15 16:56:56 -0700

    Update service provider for PHP 7.4 compatibility

--- src/Core/ServiceProvider.php
+++ src/Core/ServiceProvider.php
@@ -5,10 +5,6 @@
  * This is the base service provider class. This is an abstract class that must
  * be extended to create new service providers for the application.
  *
- * Compatible with PHP 8.0+ (typed property and return types).
- * - `void` return type introduced in PHP 7.1
- * - Typed properties introduced in PHP 7.4
- *
  * @package   Backdrop
  * @author    Benjamin Lu <benlumia007@gmail.com>
  * @copyright 2019 Benjamin Lu
@@ -29,43 +25,54 @@ use Backdrop\Contracts\Core\Application;
 abstract class ServiceProvider {
 
 	/**
-	 * Application instance. Sub-classes should use this property to access
-	 * the application (container) to add, remove, or resolve bindings.
+	 * Application instance.
+	 *
+	 * Subclasses should use this property to access the application container
+	 * to add, remove, or resolve bindings.
 	 *
 	 * @since  1.0.0
 	 * @access protected
-	 * @var    Application
+	 *
+	 * @var Application
 	 */
-	protected Application $app;
+	protected $app;
 
 	/**
 	 * Accepts the application and sets it to the `$app` property.
 	 *
 	 * @since  1.0.0
 	 * @access public
-	 * @param  Application  $app
+	 *
+	 * @param  Application $app Application instance.
 	 * @return void
 	 */
 	public function __construct( Application $app ) {
+
 		$this->app = $app;
 	}
 
 	/**
-	 * Callback executed when the `Application` class registers providers.
+	 * Callback executed when the application registers providers.
+	 *
+	 * Subclasses may override this method to register bindings, singletons,
+	 * aliases, or other services with the application container.
 	 *
 	 * @since  1.0.0
 	 * @access public
+	 *
 	 * @return void
 	 */
 	public function register(): void {}
 
 	/**
-	 * Callback executed after all the service providers have been registered.
-	 * This is particularly useful for single-instance container objects that
-	 * only need to be loaded once per page and need to be resolved early.
+	 * Callback executed after all service providers have been registered.
+	 *
+	 * Subclasses may override this method to perform initialization that
+	 * depends on services registered by other providers.
 	 *
 	 * @since  1.0.0
 	 * @access public
+	 *
 	 * @return void
 	 */
 	public function boot(): void {}
