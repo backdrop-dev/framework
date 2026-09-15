e56d89c2feaaf40ea7e693e71eaa90af2d665e71
techkodeninja <164923145+techkodeninja@users.noreply.github.com>
2026-09-15 16:46:24 -0700

    Fix bootstrap file paths

--- src/bootstrap-functions.php
+++ src/bootstrap-functions.php
@@ -2,9 +2,8 @@
 /**
  * Functions files bootstrap.
  *
- * This file loads all of our functions files necessary for using the framework.
- * Note that we are not loading class files.  Those are loaded via the autoloader
- * in `bootstrap-autoload.php`.
+ * This file loads all of our function files necessary for using the framework.
+ * Class files are loaded separately through Composer's autoloader.
  *
  * @package   Backdrop
  * @author    Benjamin Lu <benlumia007@gmail.com>
@@ -13,17 +12,19 @@
  * @link      https://github.com/backdrop-dev/framework
  */
 
-# Autoloads our custom functions files that are not loaded via the class loader.
-require_once( 'functions-filters.php'               );
-require_once( 'functions-helpers.php'               );
-require_once( 'Attr/functions-attr.php'             );
-require_once( 'Comment/functions-comment.php'       );
-require_once( 'Lang/functions-lang.php'             );
-require_once( 'Menu/functions-menu.php'             );
-require_once( 'Pagination/functions-pagination.php' );
-require_once( 'Post/functions-post.php'             );
-require_once( 'Sidebar/functions-sidebar.php'       );
-require_once( 'Site/functions-site.php'             );
-require_once( 'Template/functions-template.php'     );
-require_once( 'Theme/functions-theme.php'           );
-require_once( 'View/functions-view.php'             );
+/**
+ * Load framework function files that are not handled by the class autoloader.
+ */
+require_once __DIR__ . '/functions-filters.php';
+require_once __DIR__ . '/functions-helpers.php';
+require_once __DIR__ . '/Attr/functions-attr.php';
+require_once __DIR__ . '/Comment/functions-comment.php';
+require_once __DIR__ . '/Lang/functions-lang.php';
+require_once __DIR__ . '/Menu/functions-menu.php';
+require_once __DIR__ . '/Pagination/functions-pagination.php';
+require_once __DIR__ . '/Post/functions-post.php';
+require_once __DIR__ . '/Sidebar/functions-sidebar.php';
+require_once __DIR__ . '/Site/functions-site.php';
+require_once __DIR__ . '/Template/functions-template.php';
+require_once __DIR__ . '/Theme/functions-theme.php';
+require_once __DIR__ . '/View/functions-view.php';
