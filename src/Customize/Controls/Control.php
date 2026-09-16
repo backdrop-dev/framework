4cce527abf2dfc03b2fdbeca3d18d29eaa3212db
techkodeninja <164923145+techkodeninja@users.noreply.github.com>
2026-09-15 19:46:42 -0700

    Clean up base customize control

--- src/Customize/Controls/Control.php
+++ src/Customize/Controls/Control.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Bas customize control.
+ * Base customize control.
  *
  * This is a base customize control class for our other controls to extend.
  *
@@ -16,22 +16,23 @@ namespace Backdrop\Customize\Controls;
 use WP_Customize_Control;
 
 /**
- * Multiple checkbox customize control class.
+ * Base customize control class.
  *
  * @since  1.0.0
  * @access public
  */
 abstract class Control extends WP_Customize_Control {
 
-    /**
-     * This is the PHP callback for rendering the control content. JS-based
-     * controls require this method to be empty. Because most of our classes
-     * utilize JS templates, we're defining this in the base class to not
-     * worry about it in our subclasses.
-     *
-     * @since  1.0.0
-     * @access public
-     * @return void
-     */
-    protected function render_content() {}
+	/**
+	 * This is the PHP callback for rendering the control content. JS-based
+	 * controls require this method to be empty. Because most of our classes
+	 * utilize JS templates, we're defining this in the base class to not
+	 * worry about it in our subclasses.
+	 *
+	 * @since  1.0.0
+	 * @access protected
+	 *
+	 * @return void
+	 */
+	protected function render_content() {}
 }
