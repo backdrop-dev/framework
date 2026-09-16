91b4650190a79b65d3fbd83b013483ef59cb8575
techkodeninja <164923145+techkodeninja@users.noreply.github.com>
2026-09-15 17:31:39 -0700

    Clean up view engine contract

--- src/Contracts/View/Engine.php
+++ src/Contracts/View/Engine.php
@@ -2,19 +2,22 @@
 /**
  * Engine contract.
  *
- * Engine classes are wrappers around the View system.
+ * Defines the interface for view engines that create, display, and render
+ * view objects.
  *
  * @package   Backdrop
  * @author    Benjamin Lu <benlumia007@gmail.com>
  * @copyright 2019 Benjamin Lu
- * @link      https://github.com/backdrop-dev/framework
  * @license   https://www.gnu.org/licenses/gpl-2.0.html
+ * @link      https://github.com/backdrop-dev/framework
  */
 
 namespace Backdrop\Contracts\View;
 
+use Backdrop\Tools\Collection;
+
 /**
- * View interface.
+ * View engine interface.
  *
  * @since  1.0.0
  * @access public
@@ -22,38 +25,41 @@ namespace Backdrop\Contracts\View;
 interface Engine {
 
 	/**
-	 * Returns a View object.
+	 * Creates and returns a view object.
 	 *
 	 * @since  1.0.0
 	 * @access public
-	 * @param  string            $name
-	 * @param  array|string      $slugs
-	 * @param  array|Collection  $data
+	 *
+	 * @param  string           $name  View name.
+	 * @param  array|string     $slugs Optional view slugs.
+	 * @param  array|Collection $data  Data passed to the view.
 	 * @return View
 	 */
 	public function view( $name, $slugs = [], $data = [] );
 
 	/**
-	 * Outputs a view template.
+	 * Outputs a view.
 	 *
 	 * @since  1.0.0
 	 * @access public
-	 * @param  string            $name
-	 * @param  array|string      $slugs
-	 * @param  array|Collection  $data
+	 *
+	 * @param  string           $name  View name.
+	 * @param  array|string     $slugs Optional view slugs.
+	 * @param  array|Collection $data  Data passed to the view.
 	 * @return void
 	 */
 	public function display( $name, $slugs = [], $data = [] );
 
 	/**
-	 * Returns a view template as a string.
+	 * Renders and returns a view as a string.
 	 *
 	 * @since  1.0.0
 	 * @access public
-	 * @param  string            $name
-	 * @param  array|string      $slugs
-	 * @param  array|Collection  $data
+	 *
+	 * @param  string           $name  View name.
+	 * @param  array|string     $slugs Optional view slugs.
+	 * @param  array|Collection $data  Data passed to the view.
 	 * @return string
 	 */
-	function render( $name, $slugs = [], $data = [] );
+	public function render( $name, $slugs = [], $data = [] );
 }
